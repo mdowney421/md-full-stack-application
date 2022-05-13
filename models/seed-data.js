15b6ae0f27dca9e803cc647e96817a16e3b28269
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2022-05-13 08:49:55 -0500

    added some seed data to seed-data.js

---
+++ models/seed-data.js
@@ -0,0 +1,18 @@
+module.exports = [{
+    date: "Mon May 9 2022 08:44:59 GMT-0500 (Central Daylight Time)",
+    type: "running",
+    duration: 37,
+    caloriesBurned: 280
+},
+{
+    date: "Wed May 11 2022 08:44:59 GMT-0500 (Central Daylight Time)",
+    type: "strength training",
+    duration: 62,
+    caloriesBurned: 370
+},
+{
+    date: "Fri May 13 2022 08:44:59 GMT-0500 (Central Daylight Time)",
+    type: "swimming",
+    duration: 42,
+    caloriesBurned: 305
+}]
