d815782b05a1fbe03137f6357d214da7686ddcfb
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2022-05-16 16:33:29 -0500

    more general formatting and added required fields to forms

--- models/seed-data.js
+++ models/seed-data.js
@@ -1,18 +1,21 @@
 module.exports = [{
     date: "5/9/22",
-    type: "running",
+    type: "Running",
     duration: 37,
-    caloriesBurned: 280
+    caloriesBurned: 280,
+    notes: "ran downtown and back"
 },
 {
     date: "5/11/22",
-    type: "strength training",
+    type: "Strength Training",
     duration: 62,
-    caloriesBurned: 370
+    caloriesBurned: 370,
+    notes: "don't skip leg day!"
 },
 {
     date: "5/13/22",
-    type: "swimming",
+    type: "Swimming",
     duration: 42,
-    caloriesBurned: 305
+    caloriesBurned: 305,
+    notes: "did 30 laps in the pool"
 }]
